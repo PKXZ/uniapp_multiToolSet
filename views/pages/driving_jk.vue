eae8069359ec13b0a59d6e91b55d2df6e8e6504e
PKXZ <524572319@qq.com>
2019-07-21 20:43:28 +0800

    提交

--- views/pages/driving_jk.vue
+++ views/pages/driving_jk.vue
@@ -1,10 +1,29 @@
 <template>
 	<view class="main">
 		<view class="jk_TabTitle">
-			<view v-for="(item,index) in tabTitle" :key="item">
-				<span>{{item}}</span>
-				<p></p>
+			<view 
+			v-for="(item,index) in tabTitle" 
+			:key="index" 
+			:class="index === activedTab ? 'jk_TabTitleActive' : ''"
+			@click="clickTab(item,index)">
+				<span>{{item.name}}</span>
+				<p :class="index === activedTab ? 'jk_TabTitleActiveP' : ''"></p>
 			</view>
+		</view>
+		<view class="jk_TabMain" v-show="activedTab === 0">
+			科一
+		</view>
+		<view class="jk_TabMain" v-show="activedTab === 1">
+			科二
+		</view>
+		<view class="jk_TabMain" v-show="activedTab === 2">
+			科三
+		</view>
+		<view class="jk_TabMain" v-show="activedTab === 3">
+			科四
+		</view>
+		<view class="jk_TabMain" v-show="activedTab === 4">
+			拿本
 		</view>
 	</view>
 </template>
@@ -14,10 +33,39 @@
 	export default{
 		data(){
 			return{
-				tabTitle: ['科一','科二','科三','科四','拿本']
+				activedTab: -1,
+				tabTitle: [{
+					name: '科一',
+					type: 'kOne'
+				},{
+					name: '科二',
+					type: 'kTwo'
+				},{
+					name: '科三',
+					type: 'kThree'
+				},{
+					name: '科四',
+					type: 'kFour'
+				},{
+					name: '拿本',
+					type: 'take'
+				}],
+				activedItem: {}
 			}
 		},
-		components:{
+		watch:{
+			activedItem:{
+				handler(newData, oldData) {
+					this.activedItem = newData;
+				},
+				deep: true
+			}
+		},
+		methods:{
+			clickTab(item,index){
+				this.activedTab = index;
+				this.activedItem = item;
+			}
 		}
 	}
 </script>
